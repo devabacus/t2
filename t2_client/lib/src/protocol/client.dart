915b115dc8ecd660717fa4786295fd086cfa4d69
devabacus <32705417+devabacus@users.noreply.github.com>
2025-07-03 15:02:39 +0500

    init

---
+++ t2_client/lib/src/protocol/client.dart
@@ -0,0 +1,131 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: implementation_imports
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_literal_in_constant_pattern
+// ignore_for_file: use_super_parameters
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_client/serverpod_client.dart' as _i1;
+import 'dart:async' as _i2;
+import 'package:t2_client/src/protocol/test_data.dart' as _i3;
+import 'package:t2_client/src/protocol/greeting.dart' as _i4;
+import 'package:serverpod_auth_client/serverpod_auth_client.dart' as _i5;
+import 'protocol.dart' as _i6;
+
+/// {@category Endpoint}
+class EndpointTestData extends _i1.EndpointRef {
+  EndpointTestData(_i1.EndpointCaller caller) : super(caller);
+
+  @override
+  String get name => 'testData';
+
+  /// Создает новую запись TestData в базе данных.
+  _i2.Future<_i3.TestData> createTestData(_i3.TestData testData) =>
+      caller.callServerEndpoint<_i3.TestData>(
+        'testData',
+        'createTestData',
+        {'testData': testData},
+      );
+
+  /// Возвращает список всех записей.
+  _i2.Future<List<_i3.TestData>> listTestDatas() =>
+      caller.callServerEndpoint<List<_i3.TestData>>(
+        'testData',
+        'listTestDatas',
+        {},
+      );
+
+  /// Обновляет существующую запись.
+  _i2.Future<_i3.TestData> updateTestData(_i3.TestData testData) =>
+      caller.callServerEndpoint<_i3.TestData>(
+        'testData',
+        'updateTestData',
+        {'testData': testData},
+      );
+
+  /// Удаляет запись.
+  _i2.Future<bool> deleteTestData(_i3.TestData testData) =>
+      caller.callServerEndpoint<bool>(
+        'testData',
+        'deleteTestData',
+        {'testData': testData},
+      );
+}
+
+/// This is an example endpoint that returns a greeting message through
+/// its [hello] method.
+/// {@category Endpoint}
+class EndpointGreeting extends _i1.EndpointRef {
+  EndpointGreeting(_i1.EndpointCaller caller) : super(caller);
+
+  @override
+  String get name => 'greeting';
+
+  /// Returns a personalized greeting message: "Hello {name}".
+  _i2.Future<_i4.Greeting> hello(String name) =>
+      caller.callServerEndpoint<_i4.Greeting>(
+        'greeting',
+        'hello',
+        {'name': name},
+      );
+}
+
+class Modules {
+  Modules(Client client) {
+    auth = _i5.Caller(client);
+  }
+
+  late final _i5.Caller auth;
+}
+
+class Client extends _i1.ServerpodClientShared {
+  Client(
+    String host, {
+    dynamic securityContext,
+    _i1.AuthenticationKeyManager? authenticationKeyManager,
+    Duration? streamingConnectionTimeout,
+    Duration? connectionTimeout,
+    Function(
+      _i1.MethodCallContext,
+      Object,
+      StackTrace,
+    )? onFailedCall,
+    Function(_i1.MethodCallContext)? onSucceededCall,
+    bool? disconnectStreamsOnLostInternetConnection,
+  }) : super(
+          host,
+          _i6.Protocol(),
+          securityContext: securityContext,
+          authenticationKeyManager: authenticationKeyManager,
+          streamingConnectionTimeout: streamingConnectionTimeout,
+          connectionTimeout: connectionTimeout,
+          onFailedCall: onFailedCall,
+          onSucceededCall: onSucceededCall,
+          disconnectStreamsOnLostInternetConnection:
+              disconnectStreamsOnLostInternetConnection,
+        ) {
+    testData = EndpointTestData(this);
+    greeting = EndpointGreeting(this);
+    modules = Modules(this);
+  }
+
+  late final EndpointTestData testData;
+
+  late final EndpointGreeting greeting;
+
+  late final Modules modules;
+
+  @override
+  Map<String, _i1.EndpointRef> get endpointRefLookup => {
+        'testData': testData,
+        'greeting': greeting,
+      };
+
+  @override
+  Map<String, _i1.ModuleEndpointCaller> get moduleLookup =>
+      {'auth': modules.auth};
+}
---
+++ t2_client/lib/src/protocol/protocol.dart
@@ -0,0 +1,91 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: implementation_imports
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_literal_in_constant_pattern
+// ignore_for_file: use_super_parameters
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_client/serverpod_client.dart' as _i1;
+import 'greeting.dart' as _i2;
+import 'test_data.dart' as _i3;
+import 'package:t2_client/src/protocol/test_data.dart' as _i4;
+import 'package:serverpod_auth_client/serverpod_auth_client.dart' as _i5;
+export 'greeting.dart';
+export 'test_data.dart';
+export 'client.dart';
+
+class Protocol extends _i1.SerializationManager {
+  Protocol._();
+
+  factory Protocol() => _instance;
+
+  static final Protocol _instance = Protocol._();
+
+  @override
+  T deserialize<T>(
+    dynamic data, [
+    Type? t,
+  ]) {
+    t ??= T;
+    if (t == _i2.Greeting) {
+      return _i2.Greeting.fromJson(data) as T;
+    }
+    if (t == _i3.TestData) {
+      return _i3.TestData.fromJson(data) as T;
+    }
+    if (t == _i1.getType<_i2.Greeting?>()) {
+      return (data != null ? _i2.Greeting.fromJson(data) : null) as T;
+    }
+    if (t == _i1.getType<_i3.TestData?>()) {
+      return (data != null ? _i3.TestData.fromJson(data) : null) as T;
+    }
+    if (t == List<_i4.TestData>) {
+      return (data as List).map((e) => deserialize<_i4.TestData>(e)).toList()
+          as T;
+    }
+    try {
+      return _i5.Protocol().deserialize<T>(data, t);
+    } on _i1.DeserializationTypeNotFoundException catch (_) {}
+    return super.deserialize<T>(data, t);
+  }
+
+  @override
+  String? getClassNameForObject(Object? data) {
+    String? className = super.getClassNameForObject(data);
+    if (className != null) return className;
+    if (data is _i2.Greeting) {
+      return 'Greeting';
+    }
+    if (data is _i3.TestData) {
+      return 'TestData';
+    }
+    className = _i5.Protocol().getClassNameForObject(data);
+    if (className != null) {
+      return 'serverpod_auth.$className';
+    }
+    return null;
+  }
+
+  @override
+  dynamic deserializeByClassName(Map<String, dynamic> data) {
+    var dataClassName = data['className'];
+    if (dataClassName is! String) {
+      return super.deserializeByClassName(data);
+    }
+    if (dataClassName == 'Greeting') {
+      return deserialize<_i2.Greeting>(data['data']);
+    }
+    if (dataClassName == 'TestData') {
+      return deserialize<_i3.TestData>(data['data']);
+    }
+    if (dataClassName.startsWith('serverpod_auth.')) {
+      data['className'] = dataClassName.substring(15);
+      return _i5.Protocol().deserializeByClassName(data);
+    }
+    return super.deserializeByClassName(data);
+  }
+}
