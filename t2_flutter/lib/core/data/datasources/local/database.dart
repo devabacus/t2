1a78cf1b11dd4328d836b2a74d4c5f8541d2294c
devabacus <32705417+devabacus@users.noreply.github.com>
2025-08-04 15:06:32 +0500

    1

--- t2_flutter/lib/core/data/datasources/local/database.dart
+++ t2_flutter/lib/core/data/datasources/local/database.dart
@@ -4,25 +4,25 @@ import 'package:drift_flutter/drift_flutter.dart';
 import 'package:uuid/uuid.dart';
 import 'database_types.dart';
 
+import 'tables/sync_metadata_table.dart';
+import '../../../../features/configuration/data/datasources/local/tables/configuration_table.dart';
 // === GENERATED_IMPORTS_START ===
 import '../../../../features/home/data/datasources/local/tables/category_table.dart';
 import '../../../../features/home/data/datasources/local/tables/tag_table.dart';
 import '../../../../features/home/data/datasources/local/tables/task_table.dart';
 import '../../../../features/home/data/datasources/local/tables/task_tag_map_table.dart';
-import 'tables/sync_metadata_table.dart';
-import '../../../../features/configuration/data/datasources/local/tables/configuration_table.dart';
 // === GENERATED_IMPORTS_END ===
 
 part 'database.g.dart';
 
 @DriftDatabase(tables: [
+    SyncMetadataTable,
+    ConfigurationTable,
 // === GENERATED_TABLES_START ===
-SyncMetadataTable,
     TagTable,
     CategoryTable,
     TaskTable,
     TaskTagMapTable,
-    ConfigurationTable,
 // === GENERATED_TABLES_END ===
 ])
 class AppDatabase extends _$AppDatabase {
