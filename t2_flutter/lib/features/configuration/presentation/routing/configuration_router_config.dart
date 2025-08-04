fa46b682fa258fb6b3a27e550dcaa8984d0b0c63
devabacus <32705417+devabacus@users.noreply.github.com>
2025-08-04 10:43:29 +0500

    1

--- t2_flutter/lib/features/configuration/presentation/routing/configuration_router_config.dart
+++ t2_flutter/lib/features/configuration/presentation/routing/configuration_router_config.dart
@@ -1,6 +1,5 @@
 // lib/features/configuration/presentation/routing/configuration_router_config.dart
 import 'package:go_router/go_router.dart';
-import 'package:flutter/material.dart';
 import '../pages/configuration_page.dart';
 import 'configuration_routes_constants.dart';
 
