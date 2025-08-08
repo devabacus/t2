9465480200226ed6c14bfb4a8007a6b1483969f1
devabacus <32705417+devabacus@users.noreply.github.com>
2025-08-08 13:53:03 +0500

    1

---
+++ t2_admin/lib/features/auth/presentation/pages/auth_wrapper_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/auth_state_providers.dart';
+import 'login_page.dart';
+
+class AuthWrapperPage extends ConsumerWidget {
+  const AuthWrapperPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authStateChangesProvider);
+
+    return authState.when(
+      data: (user) {
+        if (user != null) {
+          // return const HomePage();
+          return const LoginPage();
+        } else {
+          return const LoginPage();
+        }
+      },
+      loading: () => const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      ),
+      error: (error, stackTrace) => Scaffold(
+        body: Center(
+          child: Text('Ошибка аутентификации: $error'),
+        ),
+      ),
+    );
+  }
+}
