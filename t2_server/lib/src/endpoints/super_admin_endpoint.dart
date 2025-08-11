54211a09bf022b3add862b5fa994e153c56775be
devabacus <32705417+devabacus@users.noreply.github.com>
2025-08-11 07:29:34 +0500

    1

--- t2_server/lib/src/endpoints/super_admin_endpoint.dart
+++ t2_server/lib/src/endpoints/super_admin_endpoint.dart
@@ -514,7 +514,7 @@ Future<RoleDetails?> saGetRoleDetails(Session session, UuidValue roleId) async {
   
   return RoleDetails(role: role, permissionIds: permissionIds);
 }
-
+ 
 Future<Customer?> saGetCustomer(Session session, UuidValue customerId) async {
   // Нужны проверки доступа (является ли пользователь суперадмином)
   await _requireSuperAdmin(session);
