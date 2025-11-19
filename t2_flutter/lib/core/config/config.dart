1fd33a773a43a1fb7ab13f50149228e1e7b4fd1e
devabacus <32705417+devabacus@users.noreply.github.com>
2025-11-19 05:38:04 +0500

    на web падала с ошибкой. Исправил

--- t2_flutter/lib/core/config/config.dart
+++ t2_flutter/lib/core/config/config.dart
@@ -13,13 +13,21 @@ class AppConfig {
     // return dotenv.env['BASE_URL'] ?? '';
 
     if (kDebugMode) {
-      // Проверяем, запущено ли приложение на Android
-      if (Platform.isAndroid) {
-        // Для эмулятора Android используем специальный адрес
-        return dotenv.env['LOCAL_BASE_URL_ANDROID'] ?? 'http://10.0.2.2:8080/';
-      } else {
-        // Для Windows и других настольных платформ используем localhost
+      // Сначала проверяем, является ли платформа вебом
+      if (kIsWeb) {
+        // Для веба всегда используем localhost
         return dotenv.env['LOCAL_BASE_URL_DESKTOP'] ?? 'http://localhost:8080/';
+      } else {
+        // Теперь можно безопасно использовать dart:io
+        if (Platform.isAndroid) {
+          // Для эмулятора Android используем специальный адрес
+          return dotenv.env['LOCAL_BASE_URL_ANDROID'] ??
+              'http://10.0.2.2:8080/';
+        } else {
+          // Для Windows и других настольных платформ используем localhost
+          return dotenv.env['LOCAL_BASE_URL_DESKTOP'] ??
+              'http://localhost:8080/';
+        }
       }
     } else {
       // Для релизной сборки используем удаленный сервер
